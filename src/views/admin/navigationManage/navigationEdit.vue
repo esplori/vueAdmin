fb9807d85d3649ed4e01532d0b27eb160db1e497
wilkwo <vincolarick@hotmail.com>
2021-09-26 22:00:41 +0800

    update page style

--- src/views/admin/navigationManage/navigationEdit.vue
+++ src/views/admin/navigationManage/navigationEdit.vue
@@ -9,7 +9,12 @@
       </el-form-item>
       <el-form-item label="分类：">
         <el-select v-model="form.cate">
-          <el-option v-for="(item,index) in cateList" :key=index :label="item.label" :value="item.value"></el-option>
+          <el-option
+            v-for="(item, index) in cateList"
+            :key="index"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -19,87 +24,88 @@
   </div>
 </template>
 <script>
-
-import {postPageApi, editPageApi, getDetailApi, getCateApi} from '@/views/API/navigation.js'
+import {
+  postPageApi,
+  editPageApi,
+  getDetailApi,
+  getCateApi,
+} from "@/views/API/navigation.js";
 export default {
-  data () {
+  data() {
     return {
       form: {
-        id: '',
-        title: '',
-        content: '',
-        cate: '',
-        cateName: ''
+        id: "",
+        title: "",
+        content: "",
+        cate: "",
+        cateName: "",
       },
       cateList: [
-        {label: '开发工具',value: "1"},
-        {label: '网络资讯',value: "2"},
-        {label: '资源分享',value: "3"},
-      ]
-    }
+        { label: "开发工具", value: "1" },
+        { label: "网络资讯", value: "2" },
+        { label: "资源分享", value: "3" },
+      ],
+    };
   },
-  components: {
-  },
-  mounted () {
-  },
-  computed: {
-  },
-  created () {
-    let id = this.$route.query.id
+  components: {},
+  mounted() {},
+  computed: {},
+  created() {
+    let id = this.$route.query.id;
     if (id) {
-      this.getDetail(id)
+      this.getDetail(id);
     }
     // this.getCate()
   },
   methods: {
-    async getCate () {
-      let res = await getCateApi({})
+    async getCate() {
+      let res = await getCateApi({});
       if (res) {
-        this.cateList = res.result || []
+        this.cateList = res.result || [];
         if (this.cateList.length) {
-          this.form.cate = this.cateList[0].id
+          this.form.cate = this.cateList[0].id;
         }
       }
     },
-    submit () {
+    submit() {
       if (this.form.id) {
-        this.editPage()
+        this.editPage();
       } else {
-        this.postPage()
+        this.postPage();
       }
     },
-    async editPage () {
-      this.form.cateName = this.cateList.filter(item =>{
-        return item.value === this.form.cate
-      })[0].label
-      let res = await editPageApi({...this.form})
+    async editPage() {
+      this.form.cateName = this.cateList.filter((item) => {
+        return item.value === this.form.cate;
+      })[0].label;
+      let res = await editPageApi({ ...this.form });
       if (res) {
-        this.$message.success('修改成功')
-        this.$router.push({path: '/admin/navigationList'})
+        this.$message.success("修改成功");
+        this.$router.push({ path: "/admin/navigationList" });
       }
     },
-    async postPage () {
-      this.form.cateName = this.cateList.filter(item =>{
-        return item.value === this.form.cate
-      })[0].label
-      let res = await postPageApi({...this.form})
+    async postPage() {
+      this.form.cateName = this.cateList.filter((item) => {
+        return item.value === this.form.cate;
+      })[0].label;
+      let res = await postPageApi({ ...this.form });
       if (res) {
-        this.$message.success('添加成功')
-        this.$router.push({path: '/admin/navigationList'})
+        this.$message.success("添加成功");
+        this.$router.push({ path: "/admin/navigationList" });
       }
     },
-    async getDetail (id) {
-      let res = await getDetailApi({id: id})
+    async getDetail(id) {
+      let res = await getDetailApi({ id: id });
       if (res) {
-        this.form = res.result
+        this.form = res.result;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped lang="less">
-.post{
+.post {
   width: 100%;
 }
 </style>
