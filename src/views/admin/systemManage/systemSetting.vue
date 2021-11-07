8c5f73c922a84f4038301e97d57d451c8f750917
wilkwo <2381773226@qq.com>
2021-11-07 20:37:10 +0800

    更新轮播管理

--- src/views/admin/systemManage/systemSetting.vue
+++ src/views/admin/systemManage/systemSetting.vue
@@ -22,21 +22,34 @@
       <el-form-item label="备案号:">
         <el-input v-model="form.beianNo" type="textarea"> </el-input>
       </el-form-item>
-      <el-form-item
-        v-for="(item, index) in list"
-        :key="index"
-        :label="'轮播地址' + (index + 1) + ':'"
-      >
-        <el-input
-          v-model="item.url"
-          style="width: 400px; margin-right: 10px"
-        ></el-input>
-        <el-button @click="add" v-show="index === list.length - 1"
-          >新增</el-button
-        >
-        <el-button @click="del(index)" v-show="index === list.length - 1"
-          >删除</el-button
-        >
+      <el-form-item label="轮播管理">
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column label="轮播图片地址" width="180">
+            <template slot-scope="scope">
+              <el-input v-model="scope.row.imgUrl"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column prop="name" label="轮播描述" width="180">
+            <template slot-scope="scope">
+              <el-input v-model="scope.row.desc"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column prop="address" label="跳转链接">
+            <template slot-scope="scope">
+              <el-input v-model="scope.row.url"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column prop="address" label="操作">
+            <template slot-scope="scope">
+              <el-button @click="add" v-show="scope.$index === tableData.length - 1"
+                >新增</el-button
+              >
+              <el-button @click="del($index)" v-show="scope.$index === tableData.length - 1"
+                >删除</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submit">更新</el-button>
@@ -58,9 +71,9 @@ export default {
         sourceUrl: "",
         sourceRealUrl: "",
         beianNo: "",
-        bakRealUrl: ''
+        bakRealUrl: "",
       },
-      list: [{}],
+      tableData: [{ imgUrl: "", desc: "", url: "" }],
     };
   },
   created() {
@@ -70,11 +83,7 @@ export default {
     async submit() {
       const res = await updateSiteInfoApi(
         Object.assign({}, this.form, {
-          carouselUrl: this.list
-            .map((item) => {
-              return item.url;
-            })
-            .join(","),
+          carouselUrl: JSON.stringify(this.tableData),
         })
       );
 
@@ -86,16 +95,14 @@ export default {
       const res = await getSiteInfoApi({});
       if (res) {
         this.form = res;
-        this.list = res.carouselUrl.split(",").map((item) => {
-          return { url: item };
-        });
+        this.tableData = JSON.parse(res.carouselUrl);
       }
     },
     add() {
-      this.list.push({ url: "" });
+      this.tableData.push({ imgUrl: "", desc: "", url: "" });
     },
     del(index) {
-      this.list.splice(index, 1);
+      this.tableData.splice(index, 1);
     },
   },
 };
