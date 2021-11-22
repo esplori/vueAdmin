6cc7765f4d88aac5fb51b25c07bef2ecc1cc0b4e
wilkwo <2381773226@qq.com>
2021-11-22 21:56:18 +0800

    更新登录页面样式

--- src/views/account/login.vue
+++ src/views/account/login.vue
@@ -2,64 +2,73 @@
   <div class="login">
     <div class="login-container">
       <el-form :model="form">
-      <h2 style="text-align:center;padding: 20px 0;">用户登录</h2>
-      <el-form-item>
-        <el-input v-model="form.username" placeholder="账号"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input v-model="form.password" type="password" placeholder="密码" @keyup.enter.native="login"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="login" type="primary" style="width:100%">登录</el-button>
-        <div class="reg" @click="toReg">注册</div>
-      </el-form-item>
-    </el-form>
+        <h2 style="text-align: center; padding: 20px 0">用户登录</h2>
+        <el-form-item>
+          <el-input v-model="form.username" placeholder="账号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="form.password"
+            type="password"
+            placeholder="密码"
+            @keyup.enter.native="login"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login" type="primary" style="width: 100%"
+            >登录</el-button
+          >
+          <div class="reg" @click="toReg">注册</div>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-import {loginApi} from '@/views/API/common.js'
+import { loginApi } from "@/views/API/common.js";
 export default {
-  data () {
+  data() {
     return {
       form: {
-        username: '',
-        password: ''
-      }
-    }
+        username: "",
+        password: "",
+      },
+    };
   },
   methods: {
-    async login () {
-      let res = await loginApi(this.form)
+    async login() {
+      let res = await loginApi(this.form);
       if (res) {
-        localStorage.setItem('userInfo', JSON.stringify(res))
-        this.$router.push({path: '/admin'})
+        localStorage.setItem("userInfo", JSON.stringify(res));
+        this.$router.push({ path: "/admin" });
       }
     },
     toReg() {
-      this.$router.push({path: '/reg'})
-    }
-  }
-}
+      this.$router.push({ path: "/reg" });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-  .login {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .login-container{
-      width: 300px;
-      background: #fff;
-      padding: 40px 80px;
-      box-shadow: 0 0 10px rgba(0,0,0,0.1);
-      .reg{
-        font-size: 12px;
-        text-decoration: underline;
-      }
+.login {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .login-container {
+    border-radius: 5px;
+    width: 300px;
+    background: #fff;
+    padding: 40px 80px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    .reg {
+      font-size: 12px;
+      text-decoration: underline;
     }
   }
+}
 </style>
