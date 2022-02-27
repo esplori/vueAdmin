6da4866b68cdc31d01a705505e694d6381606fdc
wilkwo <2381773226@qq.com>
2022-02-27 13:11:54 +0800

    新增在线生成二维码

--- src/views/API/tools.js
+++ src/views/API/tools.js
@@ -6,3 +6,9 @@ export async function deployApi (data) {
   const options = {}
   return post(url, data, options)
 }
+
+export async function genereateQrcodeApi (data) {
+  const url = `${contextPath}/tools/genereateQrcode`
+  const options = {}
+  return post(url, data, options)
+}
--- src/views/admin/tools/qrcode/index.vue
+++ src/views/admin/tools/qrcode/index.vue
@@ -2,42 +2,49 @@
   <div class="qrcode">
     <el-form :model="form" label-width="60px">
       <el-form-item label="文本：">
-        <el-input v-model="form.text"></el-input>
+        <el-input v-model="form.url"></el-input>
       </el-form-item>
-      <el-form-item label="logo：">
-        <el-input v-model="form.imageUrl"></el-input>
+      <el-form-item label="宽：">
+        <el-input v-model.number="form.width"></el-input>
       </el-form-item>
-      <el-form-item label="尺寸：">
-        <el-input v-model.number="form.size"></el-input>
+      <el-form-item label="高：">
+        <el-input v-model.number="form.height"></el-input>
       </el-form-item>
     </el-form>
-    <div style="text-align: center">
-      <vue-qr
-        :logoSrc="form.imageUrl"
-        :text="form.text"
-        :size="form.size"
-      ></vue-qr>
+    <div style="text-align: center;padding:20px">
+      <el-button type="primary" @click="genereateQrcode">生成二维码</el-button>
+    </div>
+    <div style="text-align: center;background: #ddd;padding:20px">
+      <img :src="qrcodeUrl" alt="">
     </div>
   </div>
 </template>
 
 <script>
+import {genereateQrcodeApi} from '../../../API/tools'
 export default {
   data() {
     return {
       form: {
-        imageUrl: require("@/assets/images/music/cover.jpeg"),
-        text: "https://www.dsiab.com",
-        size: 200,
+        url: "https://www.dsiab.com",
+        width: 300,
+        height: 300,
       },
+      qrcodeUrl: ''
     };
   },
   components: {
-    vueQr: () => import("vue-qr"),
   },
-  created() {},
+  created() {
+    // this.genereateQrcode()
+  },
   methods: {
-    generate() {},
+    async genereateQrcode() {
+      let res = await genereateQrcodeApi(this.form);
+      if (res) {
+        this.qrcodeUrl = res.data
+      }
+    },
   },
 };
 </script>
