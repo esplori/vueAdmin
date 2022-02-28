d3872c6a74fe2cfc3ca6f1e34a3cafb814961759
wilkwo <2381773226@qq.com>
2022-02-28 21:15:28 +0800

    更新二维码页面样式

--- src/views/admin/tools/qrcode/index.vue
+++ src/views/admin/tools/qrcode/index.vue
@@ -11,17 +11,17 @@
         <el-input v-model.number="form.height"></el-input>
       </el-form-item>
     </el-form>
-    <div style="text-align: center;padding:20px">
+    <div style="text-align: center; padding: 20px">
       <el-button type="primary" @click="genereateQrcode">生成二维码</el-button>
     </div>
-    <div style="text-align: center;background: #ddd;padding:20px">
-      <img :src="qrcodeUrl" alt="">
+    <div style="text-align: center; padding: 20px">
+      <img :src="qrcodeUrl" alt="" />
     </div>
   </div>
 </template>
 
 <script>
-import {genereateQrcodeApi} from '../../../API/tools'
+import { genereateQrcodeApi } from "../../../API/tools";
 export default {
   data() {
     return {
@@ -30,19 +30,14 @@ export default {
         width: 300,
         height: 300,
       },
-      qrcodeUrl: ''
+      qrcodeUrl: "",
     };
-  },
-  components: {
-  },
-  created() {
-    // this.genereateQrcode()
   },
   methods: {
     async genereateQrcode() {
       let res = await genereateQrcodeApi(this.form);
       if (res) {
-        this.qrcodeUrl = res.data
+        this.qrcodeUrl = res.data;
       }
     },
   },
@@ -50,12 +45,4 @@ export default {
 </script>
 
 <style scoped lang="less">
-.page-list {
-  width: 100%;
-  .content-item {
-    font-size: 18px;
-    text-align: left;
-    padding: 5px;
-  }
-}
 </style>
