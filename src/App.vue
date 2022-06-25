551379d456cac8703a8d33a766eadbcd4a74c597
wilkwo <2381773226@qq.com>
2022-06-25 22:45:04 +0800

    更新列表样式

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,10 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   width: 100%;
+  height: 100%;
+  overflow-y: hidden;
 }
 </style>
